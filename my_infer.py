4697115e0ae8b91062390eeb3bc353283873adc3
Jonathan Yeh <johnnyyeh0723@gmail.com>
2022-07-15 11:48:57 +0800

    Create my_infer.py

---
+++ my_infer.py
@@ -0,0 +1,65 @@
+"""OpenVINO quickly start using pre-trained model."""
+import argparse
+import cv2
+import numpy as np
+from openvino.preprocess import PrePostProcessor, ResizeAlgorithm
+from openvino.runtime import Core, Layout, Type
+
+def get_args():
+
+    # Create the parser object
+    parser = argparse.ArgumentParser()
+    
+    # Optional 
+    parser.add_argument("-p", "--photo", default = "car.png", type =str, help = "To specify target for inference.")
+
+    args = parser.parse_args()
+
+    return args
+
+def inference(args):
+    core = Core()
+    model = core.read_model('mobilenet-v2-pytorch/FP16/mobilenet-v2-pytorch.xml')
+
+    image = cv2.imread(args.photo)
+
+    input_tensor = np.expand_dims(image, 0)
+    ppp = PrePostProcessor(model)
+
+    _, h, w, _ = input_tensor.shape
+
+    (ppp.input().tensor().set_element_type(Type.u8).set_layout(
+    Layout('NHWC')).set_spatial_static_shape(h, w))
+
+    ppp.input().preprocess().resize(ResizeAlgorithm.RESIZE_LINEAR)
+    ppp.input().model().set_layout(Layout('NCHW'))
+    ppp.output().tensor().set_element_type(Type.f32)
+
+    model = ppp.build()
+    compiled_model = core.compile_model(model, 'CPU')
+
+    results = compiled_model.infer_new_request({0: input_tensor})
+    probs = next(iter(results.values()))
+    idx = np.argsort(probs[0])[::-1]
+
+    labels = "squeezenet1.1.labels.txt"
+    with open(labels, 'r') as f:
+        labels_list = f.read()
+        labels_list = labels_list.split('\n')
+
+    print("Inference Results:")
+    for i in range(5):
+        print("id: {}, target: {}, prob: {:.3f}".format(idx[i],
+                                     labels_list[idx[i]],
+                                     probs[0][idx[i]]))
+
+    cv2.imshow('target', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+def main():
+    args = get_args()
+    inference(args)
+
+if __name__ == "__main__":
+    main()
